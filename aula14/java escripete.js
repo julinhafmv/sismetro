a78b1af5fedd7704fd64f0fb5b02033f747ae579
julinhafmv <juleakajuu@gmail.com>
2023-09-21 15:56:39 -0300

    aula 14 calcu

---
+++ aula14/java escripete.js
@@ -0,0 +1,35 @@
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+function subtracao(n1, n2) {
+    return n1 - n2;
+}
+
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+
+function divisao(n1, n2) {
+    return n1 / n2;
+}
+
+function somar(){
+  document.getElementById("resultado").value = soma(Number(document.getElementById("n1").value),
+    Number(document.getElementById("n2").value) ) ;
+}
+
+function subtrair(){
+    document.getElementById("resultado").value = subtracao(Number(document.getElementById("n1").value),
+    Number(document.getElementById("n2").value)) ;
+}
+
+function dividir(){
+    document.getElementById("resultado").value = divisao(Number(document.getElementById("n1").value),
+      Number(document.getElementById("n2").value) ) ;
+  }
+
+  function multiplicar(){
+    document.getElementById("resultado").value = multiplicacao(Number(document.getElementById("n1").value),
+      Number(document.getElementById("n2").value) ) ;
+  }
